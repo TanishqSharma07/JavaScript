d8f80fa28482c96b4f8348c1b58c8e36801b5433
TanishqSharma07 <147245063+TanishqSharma07@users.noreply.github.com>
2024-05-19 11:27:32 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,4 @@
+let account_name = "Hitesh"
+let account_city = "Jaipur"
+
+console.table([account_name, account_city])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+let name12 = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+
+// number
+// string => ""
+// bool => true/false
+// null => standalone value
+// undefined 
+
+console.log(typeof(undefined))
